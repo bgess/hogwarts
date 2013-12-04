b3147f8e3fcc02804d836d05f229f3f698fdce9c
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-12-04 12:37:00 -0800

    This probably has a better chance of making a new student.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,9 +6,10 @@ class StudentsController < ActionController::Base
     @student = Student.find(params[:id])
   end
   def new
-    new_student = Student.new(parmas)
-    student = Student.create
-    house << student
+    new_student = Student.new
+    new_student.name  = params[:name]
+    new_student.house = House.find_by_name(params[:house])
+    new_student.save
     redirect to 'index'
   end
 end
