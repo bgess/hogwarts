8a0468596eebff06d5837cf161f19b41db954caa
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-12-04 12:21:44 -0800

    Seeding houses.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+House.create([{name:'Hufflepuff'}, {name:'Slitherin'}])
