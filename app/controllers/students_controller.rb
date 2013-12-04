d81b070d46933b36d0d7e1b628bf105b5089c562
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-12-04 12:26:59 -0800

    Cleanup of students controller.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,14 @@
-    class StudentsController < ApplicationController
-    def index
-    @students = Students.all
-    end
-    def show
-      @sudent = params[:student]
-    end
-    def new
-      new_student = Student.new(parmas)
-      student = Student.create
-      house << student
-      redirect to 'idnex'
-    end
+class StudentsController < ActionController::Base
+  def index
+  @students = Student.all
+  end
+  def show
+    @sudent = params[:student]
+  end
+  def new
+    new_student = Student.new(parmas)
+    student = Student.create
+    house << student
+    redirect to 'index'
+  end
+end
