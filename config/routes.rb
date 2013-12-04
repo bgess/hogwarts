dfd0782357f16e7c04c129b7e647730973ec8e21
Michelle Steigerwalt <msteigerwalt@gmail.com>
2013-12-04 12:21:53 -0800

    Added route for showing a house.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Hogwarts::Application.routes.draw do
   root to: 'welcome#index'
 
-  get '/houses' => 'houses#List'
+  get '/houses' => 'houses#index'
+  get '/houses/:id' => 'houses#show'
   get '/students' => 'students#index'
-  get '/students' => 'students#creat'
+  get '/students' => 'students#create'
 end
